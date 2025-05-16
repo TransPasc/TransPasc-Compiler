3569adec1819056438e8408fc2f43159d9d89e4c
F1star <3201125193@qq.com>
2025-05-16 22:08:18 +0800

    fix: pascal write中无分隔符

--- src/codeGenerate/cLangGenerator.cpp
+++ src/codeGenerate/cLangGenerator.cpp
@@ -913,10 +913,7 @@ std::string CLangGenerator::getCStyleIOFormatStr(
   std::string formatStr;
   for (size_t i = 0; i < types.size(); ++i) {
     formatStr += types[i]->visit(visitor);
-    if (i + 1 != types.size()) {
-      // TODO: 确定 Pascal 的分隔符
-      formatStr += " ";
-    }
+    // TODO: Pascal write 无分隔符
   }
   return formatStr;
 }
