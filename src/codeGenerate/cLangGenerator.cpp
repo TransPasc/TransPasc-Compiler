4619250afe004924f049388857417d0d1704081b
Song Jian <zhenzhen.sj@alibaba-inc.com>
2025-05-27 16:33:54 +0800

    fix: type err

--- src/codeGenerate/cLangGenerator.cpp
+++ src/codeGenerate/cLangGenerator.cpp
@@ -964,7 +964,7 @@ std::string CLangGenerator::getCStyleIOFormatStr(
           // we should use scientific format
           // so use %E with a length of 20
           // and exponential should be of length 3
-          return "%lf"s;
+          return "%f"s;
         case BasicType::BOOLEAN:
           return "%d"s;
         case BasicType::CHAR:
